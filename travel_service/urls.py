9c87a758d733bb3ac5471c5ab3140d76d5c271ec
Viesich <v.s.vesich@gmail.com>
2024-12-05 08:34:13 +0200

    create views for Station,Route,TrainType,Train,Crew,Journey,Order

---
+++ travel_service/urls.py
@@ -0,0 +1,30 @@
+from rest_framework import routers
+
+from django.urls import path, include
+
+from travel_service.views import (
+    StationViewSet,
+    RouteViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    CrewViewSet,
+    JourneyViewSet,
+    OrderViewSet,
+)
+
+app_name = "travel_service"
+
+router = routers.DefaultRouter()
+
+router.register("stations", StationViewSet)
+router.register("order", OrderViewSet)
+router.register("routes", RouteViewSet)
+router.register("train-types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("journeys", JourneyViewSet)
+# router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
