57d31355acd3eb67897f5a6524f9b7f16b09d338
Viesich <v.s.vesich@gmail.com>
2024-12-05 08:30:49 +0200

    register models for train_service app

--- travel_service/admin.py
+++ travel_service/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from travel_service.models import (
+    Ticket,
+    Train,
+    TrainType,
+    Station,
+    Route,
+    Crew,
+    Journey,
+    Order
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        TicketInline,
+    ]
+
+
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Journey)
