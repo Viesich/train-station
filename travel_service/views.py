ec2707e75086131fca2ba0890db427142ace200b
Viesich <v.s.vesich@gmail.com>
2024-12-05 08:34:37 +0200

    create views for Station,Route,TrainType,Train,Crew,Journey,Order

--- travel_service/views.py
+++ travel_service/views.py
@@ -1,3 +1,130 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from travel_service.models import (
+    Order,
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Crew,
+    Journey,
+    Ticket,
+)
+from travel_service.serializers import (
+    StationSerializer,
+    StationListSerializer,
+    StationDetailSerializer,
+    RouteSerializer,
+    TrainTypeSerializer,
+    TrainSerializer,
+    TrainListSerializer,
+    RouteListSerializer,
+    CrewSerializer,
+    CrewCreateSerializer,
+    JourneySerializer,
+    JourneyListSerializer,
+    TicketListSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+)
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Station.objects.order_by("name")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return StationListSerializer
+        if self.action == "retrieve":
+            return StationDetailSerializer
+        if self.action in ["create", "partial_update", "update"]:
+            return StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return RouteListSerializer
+        if self.action in ["create", "partial_update", "update"]:
+            return RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return TrainListSerializer
+        if self.action in ["create", "partial_update", "update"]:
+            return TrainSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CrewSerializer
+        if self.action in ["create", "partial_update", "update"]:
+            return CrewCreateSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return JourneyListSerializer
+        return JourneySerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketListSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.prefetch_related(
+        "tickets__journey__route", "tickets__journey__train"
+    )
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return OrderListSerializer
+        if self.action in ["create", "partial_update", "update"]:
+            return OrderSerializer
+        return OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = OrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        tickets_data = serializer.validated_data.get("tickets")
+        order = Order.objects.create(user=request.user)
+
+        for ticket_data in tickets_data:
+            Ticket.objects.create(order=order, **ticket_data)
+
+        return Response({"order_id": order.id}, status=status.HTTP_201_CREATED)
