d1674b63fab35a92e7ca08ef66067ae4c9c7602d
Viesich <v.s.vesich@gmail.com>
2025-01-12 19:00:38 +0200

    used variable in SECRET_KEY value

--- train_station/settings.py
+++ train_station/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w#h=^bzs&p5xt4=sb1r_w78%)8p8ugr@az3-ur=hmbu)01m&$j"
-
+SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
